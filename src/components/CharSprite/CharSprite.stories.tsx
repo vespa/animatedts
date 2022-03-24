2c6c5af7bf4a79ed5079006d3328d74ba365c9c1
Danilo Vespa <danilo.vespa@everskill.de>
2022-03-24 16:15:01 +0100

    minimal sprite animation added

--- src/components/CharSprite/CharSprite.stories.tsx
+++ src/components/CharSprite/CharSprite.stories.tsx
@@ -1,5 +1,6 @@
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 import { CharSprite } from "./CharSprite";
+import mainSprite from "static/sprites/jetpack_sprite.png";
 
 export default {
   title: "Example/CharSprite",
@@ -13,4 +14,10 @@ const Template: ComponentStory<typeof CharSprite> = (args) => (
 export const Primary = Template.bind({});
 Primary.args = {
   running: true,
+  width: 73,
+  height: 120,
+  sprite: mainSprite,
+  initialPos: 5,
+  toLeft: [8, 9],
 };
+
--- src/components/CharSprite/CharSprite.tsx
+++ src/components/CharSprite/CharSprite.tsx
@@ -1,9 +1,60 @@
+import { useEffect, useState } from "react";
+import styles from "./CharSprite.module.scss";
 interface CharSpriteProps {
   running: boolean;
+  width: number;
+  height: number;
+  sprite: string;
+  initialPos: number;
+  toLeft: number[];
 }
 
-export const CharSprite = ({ running }: CharSpriteProps) => (
-  <div>{running.toString()}</div>
-);
+export const CharSprite: React.FC<CharSpriteProps> = ({
+  running,
+  width,
+  height,
+  sprite,
+  initialPos,
+  toLeft = [],
+}) => {
+  let interval: NodeJS.Timeout;
+  //   const [animateFrames, setAnimateFrames] = useState(toLeft);
+  const [steps, setSteps] = useState<number[]>([]);
+  const [current, setCurrent] = useState(0);
+  const configureLeftAnimation = () => {
+    const stepsPos = toLeft.map((item: number) => -(item * width));
+    setSteps(stepsPos);
+  };
+
+  useEffect(() => {
+    const callNextSteps = (steps: number[], index = 0) => {
+      if (steps.length === 0) {
+        return false;
+      }
+      const step: number = steps[index];
+      interval = setTimeout(() => {
+        clearInterval(interval);
+        if (step) {
+          setCurrent(step);
+          callNextSteps(steps, index + 1);
+        } else {
+          callNextSteps(steps, 0);
+        }
+      }, 100);
+    };
+    running && callNextSteps(steps);
+  }, [steps, running]);
+
+  useEffect(() => {
+    configureLeftAnimation();
+  }, []);
+  return (
+    <div className={styles.char} style={{ width, height }}>
+      <div className={styles.char__container} style={{ left: current }}>
+        <img alt={"main char"} src={sprite} />
+      </div>
+    </div>
+  );
+};
 
 export default CharSprite;
