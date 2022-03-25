2bf048c58f450b2e8ca9438a0a30ed1512e286d3
Danilo Vespa <danilo.vespa@everskill.de>
2022-03-25 10:28:00 +0100

    back to default position command added

--- src/components/CharSprite/CharSprite.tsx
+++ src/components/CharSprite/CharSprite.tsx
@@ -17,46 +17,58 @@ export const CharSprite: React.FC<CharSpriteProps> = ({
   initialPos,
   toLeft = [],
 }) => {
-  let interval: NodeJS.Timeout;
-  //   const [animateFrames, setAnimateFrames] = useState(toLeft);
-  const [steps, setSteps] = useState<number[]>([]);
-  const [current, setCurrent] = useState(0);
-  const configureLeftAnimation = () => {
-    const stepsPos = toLeft.map((item: number) => -(item * width));
-    setSteps(stepsPos);
-  };
+    const defaultPos = -(initialPos * width);
+    let interval: NodeJS.Timeout;
+    //   const [animateFrames, setAnimateFrames] = useState(toLeft);
+    const [steps, setSteps] = useState<number[]>([]);
+    const [current, setCurrent] = useState<number>(defaultPos);
 
-  useEffect(() => {
-    const callNextSteps = (steps: number[], index = 0) => {
-      if (steps.length === 0) {
-        return false;
-      }
-      const step: number = steps[index];
-      interval = setTimeout(() => {
-        clearInterval(interval);
-        if (step) {
-          setCurrent(step);
-          callNextSteps(steps, index + 1);
-        } else {
-          callNextSteps(steps, 0);
-        }
-      }, 60);
+    const configureLeftAnimation = () => {
+      const stepsPos = toLeft.map((item: number) => -(item * width));
+      setSteps(stepsPos);
     };
-    running && callNextSteps(steps);
-    !running && clearInterval(interval);
-    return () => clearInterval(interval);
-  }, [steps, running]);
 
-  useEffect(() => {
-    configureLeftAnimation();
-  }, []);
-  return (
-    <div className={styles.char} style={{ width, height }}>
-      <div className={styles.char__container} style={{ left: current }}>
-        <img alt={"main char"} src={sprite} />
+    useEffect(() => {
+      const callNextSteps = (steps: number[], index = 0) => {
+        if (steps.length === 0) {
+          return false;
+        }
+        const step: number = steps[index];
+        interval = setTimeout(() => {
+          clearInterval(interval);
+          if (step) {
+            setCurrent(step);
+            callNextSteps(steps, index + 1);
+          } else {
+            callNextSteps(steps, 0);
+          }
+        }, 60);
+      };
+      running && callNextSteps(steps);
+      !running &&
+        (() => {
+          clearInterval(interval);
+          setCurrent(defaultPos);
+        })();
+      return () => {
+        clearInterval(interval);
+        setCurrent(defaultPos);
+      };
+    }, [steps, running]);
+
+    useEffect(() => {
+      configureLeftAnimation();
+    }, []);
+    return (
+      <div className={styles.char} style={{ width, height }}>
+        <div
+          className={styles.char__container}
+          style={{ left: current ?? defaultPos }}
+        >
+          <img alt={"main char"} src={sprite} />
+        </div>
       </div>
-    </div>
-  );
+    );
 };
 
 export default CharSprite;
