4b3c3468b8162381bff53c202dc836b2b2c6433b
D. Vespa <d.vespa@gmail.com>
2022-05-31 17:23:05 +0200

    add mouse click following

--- src/components/AnimatedSpriteNavByClick/AnimatedSpriteNavByClick.tsx
+++ src/components/AnimatedSpriteNavByClick/AnimatedSpriteNavByClick.tsx
@@ -1,22 +1,28 @@
-import { useState, useEffect, useCallback } from "react";
-
+import React, { useState, useEffect, useCallback } from "react";
 import { useSelector } from "react-redux";
 import { DirectionTypes, DirectionsNavigateKeys } from "state/action-types";
 import AnimatedSprite, { AnimatedSpriteProps } from "components/AnimatedSprite/AnimatedSprite";
 import { RootState } from "state/reducers";
+import styles from "./AnimatedSpriteNavByClick.module.scss";
+
 
 /**
  * Component that manages sprite animations. The sprite should be a horizontal sheet with tiles each one with one of the frames of the animation.
  * Each tile should have exactly the same width and the image should be perfectly centralized according to expected effect
  *
  * */
-const NavByClick: React.FC = ({ children }) => {
+type navByClickProps = {
+  className: string
+}
+const NavByClick: React.FC<navByClickProps> = ({ children, className }) => {
+  const [top, setTop] = useState<number | string>("")
+  const [left, setLeft] = useState<number | string>("")
   const { stage } = useSelector((state: RootState) => state.stage);
   const configureStage = useCallback(() => {
     stage?.addEventListener("click", (e: MouseEvent) => {
       e.preventDefault()
-      console.log("x", e.offsetY)
-      console.log("y", e.offsetX)
+      setTop(e.offsetY)
+      setLeft(e.offsetX)
     })
   }, [stage])
 
@@ -24,18 +30,18 @@ const NavByClick: React.FC = ({ children }) => {
     stage && configureStage()
   }, [stage, configureStage])
 
-  return <>{children}</>
+  return <div className={className} style={{ top, left }}>{children}</div>
 }
 
 export const AnimatedSpriteNavByClick: React.FC<AnimatedSpriteProps> = (props) => {
 
   const [direction] = useState<DirectionTypes>(
-    DirectionsNavigateKeys.LEFT
+    DirectionsNavigateKeys.DOWN
   );
   const [running] = useState<boolean>(false);
   const { id, ...rest } = props;
   return (
-    <NavByClick>
+    <NavByClick className={`${styles.char}`}>
       <AnimatedSprite {...rest} direction={direction} running={running} id={id} />
     </NavByClick>
   );
